c1ffe387b9c255bb3ea2901aadb1f0620186d580
Berthos <forgacsbert@gmail.com>
2022-05-14 20:04:30 +0300

    Add project files.

---
+++ Mail.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending email..");
+
+            Console.WriteLine("Email sent!");
+
+            Console.ReadKey();
+        }
+    }
+}
